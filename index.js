6407c9a38427a87989798e9e23e6b41d9e6e4d98
Jason Vann <vannjt1@icloud.com>
2021-06-16 23:46:45 -0400

    Completed tasks 1 - 4

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(someFood){
+      if(this.stomach.length < 10){
+        this.stomach.push(someFood);
+      }
+    }
+    poop(){
+      this.stomach = [];
+    }
+    toString(){
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
@@ -60,7 +75,19 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons){
+      this.tank = this.tank + gallons;
+    }
+    drive(distance){
+      this.odometer = this.odometer + 1;
+      this.tank = this.tank - (this.milesPerGallon / this.odometer);
+    }
   }
   
   /*
@@ -76,7 +103,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(args){
+      this.name = args.name;
+      this.age = args.age;
+      this.location = args.location;
+    }
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
   }
   
   /*
@@ -93,8 +127,22 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+  constructor(args){
+    super(args);
+    this.name = args.name;
+    this.age = args.age;
+    this.location = args.location;
+    this.specialty = args.specialty;
+    this.favLanguage = args.favLanguage;
+    this.catchPhrase = args.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
  }
   /*
     TASK 5
